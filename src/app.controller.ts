9f27e86488321c8ee59861da98c01b8a589a0f13
akshayDeq <akanojiya@deqode.com>
2022-03-01 13:10:13 +0530

    Create : Rabbitmq client service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import {
+  ClientProxy,
+  EventPattern,
+  MessagePattern,
+  Payload,
+} from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject('RABBIT_SERVICE') private readonly client: ClientProxy,
+    private readonly appService: AppService,
+  ) {}
 
   @Get()
   getHello(): string {
+    this.client.emit('cmd', { data: [1, 2, 3] });
     return this.appService.getHello();
+  }
+
+  @Post(':id')
+  async sendSome(@Param('id') id: string) {
+    this.client.emit('get-param', parseInt(id));
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'RABBIT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://user:password@localhost:5672'],
+          queue: 'userQ',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
