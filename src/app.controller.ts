912621c452b20c2a52806aa0c520eff6288cbeec
akshayDeq <akanojiya@deqode.com>
2022-03-01 13:11:08 +0530

    Create : Methods to consume data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @EventPattern('cmd')
+  async handleMessagePrinted(data: number[]) {
+    console.log(data);
+  }
+
+  @MessagePattern('get-param')
+  async getSome(data: number) {
+    console.log(data);
   }
 }
